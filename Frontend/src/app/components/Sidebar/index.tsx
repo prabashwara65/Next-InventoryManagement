83de5e03035499c3ab392dbfd4e9adb1104acf5c
prabashwara65 <prabashwara65@gmail.com>
2024-10-05 21:03:13 +0530

    commit

--- Frontend/src/app/components/Sidebar/index.tsx
+++ Frontend/src/app/components/Sidebar/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { Menu } from "lucide-react";
 import React from "react";
 
 const sidebar = () => {
@@ -6,6 +8,14 @@ const sidebar = () => {
       <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
         <div>Logo</div>
         <h1 className="font-extrabold text-2xl">EDSTOCKS</h1>
+        <button className="md:hidden px-3 py-3 bg-gray-300 rounded-full hover:bg-blue-100" onClick={() => {}}>
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+     
+      <div className="flex-grow mt-8"> {/* Links */}</div>
+      <div>
+        <p className="text-center text-xs text-green-500">&copy; 2024 EDSTOCKS</p>
       </div>
     </div>
   );
