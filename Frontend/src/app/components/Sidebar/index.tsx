8dca9265c10aa39f306086ef04bcb23847dbc167
prabashwara65 <prabashwara65@gmail.com>
2024-10-05 16:31:22 +0530

    commit

---
+++ Frontend/src/app/components/Sidebar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const sidebar = () => {
+  return (
+    <div>
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>Logo</div>
+        <h1 className="font-extrabold text-2xl">EDSTOCKS</h1>
+      </div>
+    </div>
+  );
+};
+
+export default sidebar;
